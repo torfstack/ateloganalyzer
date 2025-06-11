2de162a939b32fa15e79194692a4d24a70d88b0c
David Deininger <torfstack@gmail.com>
2025-06-11 21:49:53 +0200

    refactor: use regex in input log file parsing instead of string splits+comparisons (more robust)

--- app/src/main/java/com/torfstack/ateloganalyzer/input/Input.java
+++ app/src/main/java/com/torfstack/ateloganalyzer/input/Input.java
@@ -10,12 +10,17 @@ import java.time.Duration;
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Stream;
 
 import static com.torfstack.ateloganalyzer.constants.Constants.TIME_FORMATTER;
 
 public class Input {
+
+    private static final Pattern TIMESTAMP_PATTERN = Pattern.compile("#PE\\[(.+)]");
+    private static final Pattern DEVICE_PATTERN = Pattern.compile("BEGIN INSERTION \"(.+)\"");
+    private static final Pattern SUBTEST_NAME_PATTERN = Pattern.compile("DEVICE_TEST.SUBTEST \"(.+)\"");
 
     private static class ParseContext {
         String currentDevice = "Unknown Device";
@@ -25,56 +30,35 @@ public class Input {
 
     public static @NonNull List<TestEvent> parseLogFile(@NonNull String filename) throws IOException {
         List<TestEvent> events = new ArrayList<>();
+        ParseContext context = new ParseContext();
 
         try (Stream<String> lines = Files.lines(Paths.get(filename))) {
-            ParseContext context = new ParseContext();
             lines.forEach(line -> {
-                line = line.trim();
-                if (isValidLine(line)) {
-                    parseEvent(line, context).ifPresent(events::add);
+                Matcher deviceMatcher = DEVICE_PATTERN.matcher(line);
+                context.currentDevice = deviceMatcher.find() ? deviceMatcher.group(1) : context.currentDevice;
+
+                Matcher subtestMatcher = SUBTEST_NAME_PATTERN.matcher(line);
+                context.currentSubtest = subtestMatcher.find() ? subtestMatcher.group(1) : context.currentSubtest;
+
+                Matcher timestampMatcher = TIMESTAMP_PATTERN.matcher(line);
+                String timestampString = timestampMatcher.find() ? timestampMatcher.group(1) : null;
+                if (timestampString == null) return;
+                LocalTime timestamp = LocalTime.parse(timestampString, TIME_FORMATTER);
+
+                boolean isBeginSubtest = line.contains("BEGIN DEVICE_TEST.SUBTEST");
+                boolean isEndSubtest = line.contains("END DEVICE_TEST.SUBTEST");
+
+                if (isBeginSubtest) {
+                    context.currentBeginTimestamp = timestamp;
+                } else if (isEndSubtest) {
+                    TestEvent event = new TestEvent(context.currentBeginTimestamp, timestamp,
+                            Duration.between(context.currentBeginTimestamp, timestamp),
+                            context.currentSubtest, context.currentDevice);
+                    events.add(event);
                 }
             });
         }
 
         return events;
-    }
-
-    private static boolean isValidLine(@NonNull String line) {
-        return !line.trim().isEmpty() && line.startsWith("#PE");
-    }
-
-    private static @NonNull Optional<TestEvent> parseEvent(@NonNull String line, @NonNull ParseContext context) {
-        int timeStart = line.indexOf('[') + 1;
-        int timeEnd = line.indexOf(']');
-        String timeStr = line.substring(timeStart, timeEnd);
-        LocalTime timestamp = LocalTime.parse(timeStr, TIME_FORMATTER);
-
-        int descStart = line.indexOf(": ") + 2;
-        String description = line.substring(descStart);
-
-        String deviceInfo = description.contains("\"device") ?
-                description.substring(description.indexOf("\"")+1, description.lastIndexOf("\"")) : "";
-        if (!deviceInfo.isEmpty()) {
-            context.currentDevice = deviceInfo;
-        }
-
-        // not an event of Step 2, we only care about the subtest events
-        if (!description.contains("DEVICE_TEST.SUBTEST")) {
-            return Optional.empty();
-        }
-
-        boolean isBegin = description.startsWith("BEGIN");
-        if (!isBegin) {
-            Duration duration = Duration.between(context.currentBeginTimestamp, timestamp);
-            TestEvent endEvent = new TestEvent(context.currentBeginTimestamp, timestamp, duration, context.currentSubtest, context.currentDevice);
-            context.currentSubtest = "";
-            context.currentBeginTimestamp = null;
-            return Optional.of(endEvent);
-        }
-
-        context.currentSubtest =
-                description.substring(description.indexOf("SUBTEST") + 9, description.lastIndexOf("\""));
-        context.currentBeginTimestamp = timestamp;
-        return Optional.empty();
     }
 }
